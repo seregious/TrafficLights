9e2f343a55bbdbb7e2ef58b859e9ff3f52e9a7a2
seregious <seregious@gmail.com>
2022-04-03 17:34:45 +0300

    setup logic

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    var count = 1
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.alpha = 1
+        redLight.layer.cornerRadius = 75
+        
+        yellowLight.alpha = 0.2
+        yellowLight.layer.cornerRadius = 75
+        
+        greenLight.alpha = 0.2
+        greenLight.layer.cornerRadius = 75
+        
     }
+    
+    
 
-
+    @IBAction func switchColor() {
+        count += 1
+        if count == 4 {
+            count = 1
+        }
+        
+        switch count {
+        case 1:
+            redLight.alpha = 1
+            greenLight.alpha = 0.2
+        case 2:
+            redLight.alpha = 0.2
+            yellowLight.alpha = 1
+        case 3:
+            yellowLight.alpha = 0.2
+            greenLight.alpha = 1
+        default:
+            break
+        }
+    }
+    
 }
 
